bc04edd2309dbce31e02ea3294ed870cd8f3094d
lindo <112756741+lindo20@users.noreply.github.com>
2023-04-06 02:28:37 +0200

    Add files via upload

---
+++ module4-assignment/new.js
@@ -0,0 +1,10 @@
+const names = ['Yaakov','John','jen','jason','Paul','Frank','Larry','Paula','Laura','jim'];
+
+names.forEach(name => {
+	if(name.startsWith('j')|| name.startsWith('J')){
+		console.log(`Goodbye, ${name}!`);
+	}
+	else{
+		console.log(`Goodmorning, ${name}!`);
+	}
+});
